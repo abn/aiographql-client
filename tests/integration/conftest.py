b86482d11e823f32dc3b1b0192a4800dcbb2e961
Maria Soulountsi <mariasoul01@gmail.com>
2019-07-24 12:19:53 +0200

    Extend test coverage to cover basic use cases
    Implement new test cases to verify base functionality of the library
    against the hasura-world-db sample server.

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,7 +1,100 @@
+import uuid
+
+import pytest
+
+from aiographql.client.client import GraphQLClient
+from aiographql.client.transaction import GraphQLRequest
+
+
 def pytest_addoption(parser):
     parser.addoption(
-        "--pokemon-server",
+        "--world-server",
         action="store",
-        default="http://localhost:5000",
+        default="http://127.0.0.1:8080/v1/graphql",
         help="GraphQL server to use for integration tests",
     )
+
+
+@pytest.fixture
+def headers():
+    return {"x-hasura-admin-secret": "secret"}
+
+
+@pytest.fixture
+def server(request):
+    return request.config.getoption("--world-server")
+
+
+@pytest.fixture(autouse=True)
+def client(server) -> GraphQLClient:
+    return GraphQLClient(endpoint=server)
+
+
+@pytest.fixture(scope="module")
+def city_name():
+    return str(uuid.uuid4())
+
+
+@pytest.fixture
+def subscription_query(city_name):
+    return f"""
+        subscription {{
+          city(where: {{name: {{_eq: "{city_name}"}}}}) {{
+            name
+            id
+          }}
+        }}
+    """
+
+
+@pytest.fixture
+def query_city():
+    return """
+        query{
+          city(where: {name: {_eq: "Groningen"}}) {
+            name
+            id
+          }
+        }
+        """
+
+
+@pytest.fixture
+def query_output():
+    return {"city": [{"id": 11, "name": "Groningen"}]}
+
+
+@pytest.fixture
+def invalid_query():
+    return """
+        query{
+          city(where: {name: {_eq: "Groningen"}}) {
+            name
+            id
+        }
+        """
+
+
+@pytest.fixture
+async def mutation_city(client, headers, city_name):
+    yield f"""
+        mutation {{
+          insert_city(objects: {{id: 4081, name: "{city_name}", population: 10, country_code: "GRC", district: "Greece"}}) {{
+            affected_rows
+          }}
+        }}
+    """
+    delete_mutation = f"""
+        mutation {{
+          delete_city(where: {{name: {{_eq: "{city_name}"}}}}) {{
+            affected_rows
+          }}
+        }}
+        """
+    request = GraphQLRequest(query=delete_mutation)
+    _ = await client.query(request=request, headers=headers)
+
+
+@pytest.fixture
+def mutation_output():
+    return {"insert_city": {"affected_rows": 1}}
