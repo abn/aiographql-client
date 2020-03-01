e86a4683a2cc32b8548463a3f1efbc4e89e9cb3e
Arun Babu Neelicattu <arun.neelicattu@twyla.ai>
2020-03-01 21:21:17 +0100

    Add support for locations and path properties for error payload

--- aiographql/client/error.py
+++ aiographql/client/error.py
@@ -1,8 +1,10 @@
 from dataclasses import dataclass, field
-from typing import Dict, Any, Optional
+from typing import Dict, Any, Optional, List, Union
 
 
 @dataclass(frozen=True)
 class GraphQLError:
     extensions: Dict[str, Any] = field(default_factory=dict)
+    locations: Optional[List[Dict[str, int]]] = field(default=None)
     message: Optional[str] = field(default=None)
+    path: Optional[List[Union[str, int]]] = field(default=None)
