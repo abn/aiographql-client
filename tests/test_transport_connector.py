c38974b2f7d0b66162b775802d2e9a094532b447
Arun Babu Neelicattu <arun.neelicattu@gmail.com>
2026-05-26 23:18:57 +0200

    test(transport): add test for AiohttpTransport.create_default_connector

---
+++ tests/test_transport_connector.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+
+import aiohttp
+import pytest
+
+from aiographql.client.transport.aiohttp import AiohttpTransport
+
+
+@pytest.mark.asyncio
+async def test_create_default_connector() -> None:
+    connector = await AiohttpTransport.create_default_connector()
+    assert isinstance(connector, aiohttp.TCPConnector)
+    assert connector.force_close is True
+    assert connector.limit == 100
+    # Clean up the connector
+    await connector.close()
